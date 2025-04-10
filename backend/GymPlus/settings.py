4c942bf55744691422839182b30d51e2b3e79ae3
Paola Zuniga <pazuniga@unicesmag.edu.co>
2025-04-10 14:51:51 -0500

    Configuraciones Postgresql

--- backend/GymPlus/settings.py
+++ backend/GymPlus/settings.py
@@ -79,11 +79,11 @@ WSGI_APPLICATION = 'GymPlus.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'dbgymplus',
-        'USER': 'postgres',
-        'PASSWORD': '123',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT', '5432'),
     }
 }
 
