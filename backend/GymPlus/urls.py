2d2976939ee0b3274a3d5252c6fafa573c7299af
Paola Zuniga <pazuniga@unicesmag.edu.co>
2025-04-10 14:32:17 -0500

    Configuraciones Deploy Vercel2

--- backend/GymPlus/urls.py
+++ backend/GymPlus/urls.py
@@ -20,11 +20,6 @@ from django.views.generic import TemplateView
 from django.views.decorators.cache import never_cache
 
 
-
-
-from django.contrib import admin
-from django.urls import path, include
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('miembros.urls')),  # <-- Cambia 'tu_app' por el nombre real de tu app
