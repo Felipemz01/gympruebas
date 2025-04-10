2d31ca1760a74455ecdeb6864e3430ad5fe95a0a
Paola Zuniga <pazuniga@unicesmag.edu.co>
2025-04-10 17:54:25 -0500

    Configuraciones Deploy Vercel7

--- frontend/src/app/services/registro.service.ts
+++ frontend/src/app/services/registro.service.ts
@@ -1,12 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'  // Hace que este servicio esté disponible globalmente
 })
 export class RegistroService {
-  private apiUrl = 'http://127.0.0.1:8000/api/miembros/';
+  /*private apiUrl = 'http://127.0.0.1:8000/api/miembros/';*/
+  private apiUrl = `${environment.apiUrl}/api/miembros/`; // 🔄 Usamos la URL del entorno
 
   constructor(private http: HttpClient) {}
 
